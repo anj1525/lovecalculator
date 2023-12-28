2babb9cebbd7e12d9a48f70568f5f059b992eb57
anj1525 <anjanisharm@outlook.com>
2023-12-27 21:40:00 -0500

    Did most of the code, going to work a bit more on the UI

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    let button = document.getElementById("calculate");
+    button.addEventListener("click", calculateLove);
+}
+
+function calculateLove() {
+    let yourName = document.getElementById("your-name").value;
+    let crushName = document.getElementById("crush-name").value;
+
+    if (yourName !== "" && crushName !== "") {
+        let percentage;
+    
+        if (yourName.toLowerCase() === "anjani" && crushName.toLowerCase() === "casey") {
+            percentage = 100; 
+        } else {
+            percentage = Math.floor(Math.random() * 101);
+        }
+    
+        document.getElementById("result-message").innerText = yourName + " and " + crushName + "'s chance of love:";
+        document.getElementById("result-percentage").innerText = percentage.toString() + "%";
+    }
+    
+}    
